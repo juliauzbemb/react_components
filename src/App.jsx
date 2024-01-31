350521230dfc43674e916aa35d7ffcc33ff652b6
juliauzbemb <90088899+juliauzbemb@users.noreply.github.com>
2024-01-31 18:19:00 +0300

    ready

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './components/ShopItemClass.css';
+import './App.css';
+
+import { item, ShopItemClass } from './components/ShopItemClass';
+
+
+function App() {
+  return (
+    <>
+    <div className="container">
+      <div className="background-element">
+      </div>
+      <div className="highlight-window">
+        <div className='highlight-overlay'></div>
+      </div>
+      <div className="window">
+        <ShopItemClass item={item} />
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
